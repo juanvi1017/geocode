be1158749139fa9682bae9385f6671a38fca38e0
Juanvi <jvcaceres17@gmail.com>
2023-08-26 11:45:00 -0500

    geocode search

---
+++ geo.py
@@ -0,0 +1,68 @@
+import googlemaps
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+if __name__ == '__main__':
+    # Params conection
+    nameNeighbor = ''
+    iteration = 0
+    addressNumber = 1300
+    address = ' SE Stark St, Portland, OR 97214'
+    args = {
+        'timeRelation': 'esriTimeRelationOverlaps',
+        'geometry': '',
+        'geometryType': 'esriGeometryPoint',
+        'inSR': '4326',
+        'spatialRel': 'esriSpatialRelIntersects',
+        'units': 'esriSRUnit_Foot',
+        'outFields': '*',
+        'returnGeometry': 'false',
+        'featureEncoding': 'esriDefault',
+        'f': 'json'
+    }
+
+    def geocode():
+        global iteration
+        global addressNumber
+        global address
+        global nameNeighbor
+        global args
+
+        if iteration > 0:
+            addressNumber = addressNumber + 100
+            iteration = iteration + 1
+        gmaps = googlemaps.Client(key=os.environ.get("KEY"))
+
+        # Geocoding an address
+        geocode_result = gmaps.geocode(str(addressNumber) + address)
+        result = geocode_result[0]['geometry']['location']
+        if result:
+            args['geometry'] = str(result['lng']) + ', ' + str(result['lat'])
+
+        response = requests.get(os.environ.get("URLGEO"), params=args)
+        if response.status_code == 200:
+            data = response.json()
+            if len(nameNeighbor) < 1:
+                iteration += 1
+                nameNeighbor = data['features'][0]['attributes']['NAME']
+                print('Vecindario localizado ' + nameNeighbor)
+                geocode()
+            else:
+                if data['features'][0]['attributes']['NAME'] == nameNeighbor:
+                    print('IGUAL ' + nameNeighbor)
+                    geocode()
+                else:
+                    print('DIFRENTE')
+                    print(str(addressNumber) + address)
+                    print(args['geometry'])
+                    print(data['features'][0]['attributes']['NAME'])
+        else:
+            print('Error conexion')
+
+    geocode()
+
+
+    #ADJUNTE ARCHIVO .ENV
